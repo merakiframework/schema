a43107e7ac5875d5bf677c1a9d0fa7164f58af87
Nathan <nbish11@hotmail.com>
2024-12-04 22:51:49 +0000

    Use named field type creation methods and add rule example

--- examples/serialize.php
+++ examples/serialize.php
@@ -2,22 +2,52 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$schema = new Meraki\Form\Schema();
+use Meraki\Schema\Outcome;
+use Meraki\Schema\Rule;
+use Meraki\Schema\Rule\Condition;
 
-$schema->add(
-	(new Meraki\Form\Field('username', 'text'))
-		->require()
-		->minLengthOf(3)
-		->maxLengthOf(20)
-);
+$schema = new Meraki\Schema\SchemaFacade('contact_form');
 
-$schema->add(
-	(new Meraki\Form\Field('age', 'number'))
-		->require()
-		->minOf(18)
-		->maxOf(120)
+$name = $schema->addNameField('name')
+	->require();
+
+$contactMethod = $schema->addEnumField('contact_method', ['email', 'phone'])
+	->require();
+
+$message = $schema->addTextField('message')
+	->require()
+	->minLengthOf(10)
+	->maxLengthOf(500)
+	->allowMultipleLines();
+
+$emailAddress = $schema->addEmailAddressField('email_address');
+
+$phoneNumber = $schema->addPhoneNumberField('phone_number');
+
+
+$schema->addRule(
+	new Rule(
+		Condition::matchAny(
+			Condition::matchAll(
+				Condition::create('#/field_1/value', 'equals', 'value1'),
+				Condition::create('#/field_2/value', 'equals', 'value2'),
+				Condition::matchAll(
+					Condition::create('#/field_3/value', 'equals', 'value3'),
+					Condition::create('#/field_4/value', 'notEquals', 'value4'),
+				),
+			),
+
+			Condition::matchNone(
+				Condition::create('#/contact_method/value', 'equals', 'phone'),
+			),
+		),
+		[
+			Outcome::doStuff('#/a_field'),
+			Outcome::doOtherStuff('#/another_field')
+		]
+	)
 );
 
 echo '<pre>';
-echo $schema->serialize(new Meraki\Form\Serializer\Json());
+echo $schema->serialize();
 echo '</pre>';
