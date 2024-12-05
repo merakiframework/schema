876a5f40b5178ad8f3c587afa7cd78354f178db7
Nathan <nbish11@hotmail.com>
2024-12-05 01:27:21 +0000

    Move logic to appropriate condition/outcome groups

--- src/SchemaValidator.php
+++ src/SchemaValidator.php
@@ -3,12 +3,7 @@ declare(strict_types=1);
 
 namespace Meraki\Schema;
 
-use Meraki\Schema\AggregatedValidationResults;
 use Meraki\Schema\SchemaFacade;
-use Meraki\Schema\Rule\ConditionGroup;
-use Meraki\Schema\Outcome;
-use Meraki\Schema\SchemaValidationResult;
-use Meraki\Schema\Field;
 use Meraki\Schema\Attribute;
 
 final class SchemaValidator
@@ -34,14 +29,7 @@ final class SchemaValidator
 			$fieldResult = $field->input($data[(string)$field->name] ?? null);
 		}
 
-		// evaluate rules
-		foreach ($this->schema->rules as $rule) {
-			if ($rule->evaluate($data, $this->schema)) {
-				foreach ($rule->then as $outcome) {
-					$this->applyOutcome($outcome, $results, $data, $this->schema);
-				}
-			}
-		}
+		$this->schema->rules->apply($data, $this->schema);
 
 		// validate fields
 		foreach ($this->schema->fields as $field) {
@@ -50,50 +38,5 @@ final class SchemaValidator
 		}
 
 		return $results;
-	}
-
-	private function applyOutcome(
-		Outcome $outcome,
-		AggregatedFieldValidationResults $validationResult,
-		array $data,
-		SchemaFacade $schema,
-	): void {
-		$target = $outcome->target;
-		$action = $outcome->action;
-
-		switch ($action->value) {
-			case 'require':
-				$target = $target->getScope()->resolve($schema);
-				if (!($target instanceof Field)) {
-					throw new \RuntimeException('Cannot require non-field');
-				}
-				$target->require();
-				break;
-
-			case 'set':
-				if (!isset($outcome->to)) {
-					throw new \RuntimeException('Missing "to" value for "set" action');
-				}
-				$scope = $target->getScope();
-				$target = $scope->resolveWithBackTracking($schema);
-				if (!($target instanceof Field)) {
-					throw new \RuntimeException('Cannot set attribute on non-field');
-				}
-				$attr = $this->attributeFactory->create($scope->getLastSegment(), $outcome->to->value);
-				$target->setAttribute($attr);
-				break;
-
-			case 'replace':
-				if (!isset($outcome->with)) {
-					throw new \RuntimeException('Missing "with" value for "replace" action');
-				}
-				$scope = $target->getScope();
-				$target = $scope->resolve($schema);
-				throw new \RuntimeException('Not implemented yet');
-				// break;
-
-			default:
-				throw new \RuntimeException('Unknown action: ' . $action->value);
-		}
 	}
 }
