deff5546fe05b4c39c7655bbd50f278d8e5ac7fa
Nathan <nbish11@hotmail.com>
2024-12-05 04:40:20 +0000

    Simplified example

--- examples/validate.php
+++ examples/validate.php
@@ -2,21 +2,17 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$schema = new Meraki\Form\Schema();
+$schema = new Meraki\Schema\SchemaFacade('contact_form');
 
-$schema->add(
-	(new Meraki\Form\Field('username', 'text'))
-		->require()
-		->minLengthOf(3)
-		->maxLengthOf(20)
-);
+$schema->addTextField('username')
+	->require()
+	->matches('/^[a-zA-Z0-9_]+$/')
+	->minLengthOf(3);
 
-$schema->add(
-	(new Meraki\Form\Field('age', 'number'))
-		->require()
-		->minOf(18)
-		->maxOf(120)
-);
+$schema->addNumberField('age')
+	->require()
+	->minOf(18)
+	->maxOf(120);
 
 $validUserData = [
 	'username' => 'johndoe',
@@ -24,7 +20,7 @@ $validUserData = [
 ];
 
 $invalidUserData = [
-	'username' => 'jd',
+	'username' => '',
 	'age' => 15,
 ];
 
@@ -32,14 +28,20 @@ $userData = isset($_GET['pass']) ? $validUserData : $invalidUserData;
 $result = $schema->validate($userData);
 
 echo '<pre>';
-if ($result->passed()) {
+if ($result->allPassed()) {
 	echo 'The data is valid.';
 } else {
-	foreach ($result->errors as $fieldName => $errors) {
-		echo 'The data is invalid for field "' . $fieldName . '":' . PHP_EOL;
+	foreach ($result->getFailures() as $fieldFailures) {
+		if ($fieldFailures->valueValidationResult->passed()) {
+			echo "Field '{$fieldFailures->field->name}' passed." . PHP_EOL;
+			echo "Validating constraints..." . PHP_EOL;
 
-		foreach ($errors as $error) {
-			echo '    - ' . $error . PHP_EOL;
+			foreach ($fieldFailures->constraintValidationResults as $constraintFailure) {
+				echo "'{$constraintFailure->constraint->name}' constraint failed." . PHP_EOL;
+			}
+		} else {
+			echo "Field '{$fieldFailures->field->name}' failed." . PHP_EOL;
+			echo "Skipping constraint validation..." . PHP_EOL;
 		}
 
 		echo PHP_EOL;
