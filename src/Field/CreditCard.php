c8999519bce4d9bbd23c248fa625317e5f3d96a0
Nathan <nbish11@hotmail.com>
2024-12-25 10:36:45 +1000

    Fix credit-card field type having incorrect field type

--- src/Field/CreditCard.php
+++ src/Field/CreditCard.php
@@ -12,7 +12,7 @@ class CreditCard extends Field
 {
 	public function __construct(Attribute\Name $name, Attribute ...$attributes)
 	{
-		parent::__construct(new Attribute\Type('name'), $name, ...$attributes);
+		parent::__construct(new Attribute\Type('credit-card'), $name, ...$attributes);
 	}
 
 	public static function getSupportedAttributes(): array
