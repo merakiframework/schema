8c375e6b2070f6c06a562eb5628dc50a08fb10e5
Nathan <nbish11@hotmail.com>
2024-12-25 10:37:02 +1000

    Fix date field type having incorrect field type

--- src/Field/Date.php
+++ src/Field/Date.php
@@ -10,7 +10,7 @@ class Date extends Field
 {
 	public function __construct(Attribute\Name $name, Attribute ...$attributes)
 	{
-		parent::__construct(new Attribute\Type('name'), $name, ...$attributes);
+		parent::__construct(new Attribute\Type('date'), $name, ...$attributes);
 	}
 
 	public static function getSupportedAttributes(): array
