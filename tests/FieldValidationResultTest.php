887871745be3fc2aa41869725bfe0d4162bc8c3f
Nathan Bishop <nbish11@hotmail.com>
2025-01-12 18:51:08 +1100

    Add some basic test cases

--- tests/FieldValidationResultTest.php
+++ tests/FieldValidationResultTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace Meraki\Schema;
 
 use Meraki\Schema\FieldValidationResult;
+use Meraki\Schema\AggregatedValidationResults;
+use Meraki\Schema\ValidationResult;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\{Test, CoversClass};
 
@@ -14,5 +16,34 @@ final class FieldValidationResultTest extends TestCase
 	public function it_exists(): void
 	{
 		$this->assertTrue(class_exists(FieldValidationResult::class));
+	}
+
+	#[Test]
+	public function it_is_a_validation_result(): void
+	{
+		$result = new FieldValidationResult();
+
+		$this->assertInstanceOf(ValidationResult::class, $result);
+	}
+
+	#[Test]
+	public function it_is_an_aggregation_of_validation_results(): void
+	{
+		$result = new FieldValidationResult();
+
+		$this->assertInstanceOf(AggregatedValidationResults::class, $result);
+	}
+
+	#[Test]
+	public function it_is_in_the_pending_state_if_no_results(): void
+	{
+		$result = new FieldValidationResult();
+
+		$this->assertCount(0, $result);
+		$this->assertTrue($result->pending());
+		$this->assertFalse($result->passed());
+		$this->assertFalse($result->failed());
+		$this->assertFalse($result->skipped());
+		$this->assertEquals(ValidationResult::PENDING, $result->status);
 	}
 }
