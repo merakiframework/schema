0f486f55e3687d7ad2589964a5dc275f72d11f17
Nathan Bishop <nbish11@hotmail.com>
2025-04-22 08:47:35 +1000

    Simplify interface

--- src/ValidationResult.php
+++ src/ValidationResult.php
@@ -8,29 +8,4 @@ namespace Meraki\Schema;
  */
 interface ValidationResult
 {
-	public const PASSED = 0;
-	public const SKIPPED = 1;
-	public const FAILED = 2;
-
-	public const PENDING = 3;
-
-	/**
-	 * Check if the validation result has passed.
-	 */
-	public function passed(): bool;
-
-	/**
-	 * Check if the validation result was was skipped.
-	 */
-	public function skipped(): bool;
-
-	/**
-	 * Check if the validation result has failed.
-	 */
-	public function failed(): bool;
-
-	/**
-	 * Check if the validation result is pending.
-	 */
-	public function pending(): bool;
 }
--- tests/ValidationResultTestCase.php
+++ tests/ValidationResultTestCase.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 namespace Meraki\Schema;
 
 use Meraki\Schema\ValidationResult;
+use Meraki\Schema\ValidationStatus;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\Group;
 
+#[Group('validation')]
 #[CoversClass(ValidationResult::class)]
 abstract class ValidationResultTestCase extends TestCase
 {
-	#[Test]
-	abstract public function it_exists(): void;
-
 	#[Test]
 	public function it_is_a_validation_result(): void
 	{
@@ -23,22 +23,12 @@ abstract class ValidationResultTestCase extends TestCase
 	}
 
 	#[Test]
-	public function can_create_a_new_instance_that_passed(): void
+	public function it_has_a_status(): void
 	{
 		$result = $this->createValidationResult();
 
-		$this->assertTrue($result->passed());
-		$this->assertFalse($result->failed());
+		$this->assertInstanceOf(ValidationStatus::class, $result->status);
 	}
 
-	#[Test]
-	public function can_create_a_new_instance_that_failed(): void
-	{
-		$result = $this->createValidationResult(ValidationResult::FAILED);
-
-		$this->assertFalse($result->passed());
-		$this->assertTrue($result->failed());
-	}
-
-	abstract public function createValidationResult(int $status = ValidationResult::PASSED): ValidationResult;
+	abstract public function createValidationResult(): ValidationResult;
 }
