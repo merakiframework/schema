242164e84a8508c87cc1085298840466cb2db5d3
Nathan Bishop <nbish11@hotmail.com>
2025-04-22 08:21:17 +1000

    Make a 'field name' not a field

--- src/Field/Name.php
+++ src/Field/Name.php
@@ -3,77 +3,36 @@ declare(strict_types=1);
 
 namespace Meraki\Schema\Field;
 
-use Meraki\Schema\Field;
-use Meraki\Schema\Attribute;
-use Meraki\Schema\Constraint;
-use Meraki\Schema\Validator;
-
 /**
- * A "name" field is used to represent a person's full name.
+ * A "name" attribute.
  *
- * It does not make any assumptions about the structure of a name.
- * There are, however, some sane restrictions. A name can:
- *
- * 	- contain unicode letters, spaces, apostrophes, periods, and dashes
- * 	- consist of one or more "words" separated by spaces
- * 	- each "word" must be at least one character long
- *  - should use Roman Numerals to represent numbers (e.g. John Doe IV)
- *
- * You can override these restrictions by setting the appropriate attributes.
- *
- * @see https://www.w3.org/International/questions/qa-personal-names
- * @see https://shinesolutions.com/2018/01/08/falsehoods-programmers-believe-about-names-with-examples/
+ * The name attribute is used to specify the name of a field.
  */
-class Name extends Field
+final class Name
 {
-	/**
-	 * - allow unicode letters, spaces, apostrophes, periods, commas, and dashes
-	 * - allow an empty string
-	 * - all "words" must be separated by one or more spaces
-	 * - each "word" must be at least one character long
-	 */
-	private const TYPE_PATTERN = "/^(?![\ \.\,\'\-]+$)[\p{L}\.\,\'\ \-]*$/u";
+	public string $name = 'name';
+	public string $prefix = '';
 
-	public function __construct(Attribute\Name $name, Attribute ...$attributes)
+	public function __construct(public readonly string $value)
 	{
-		parent::__construct(new Attribute\Type('name'), $name, ...$attributes);
-
-		$this->registerConstraints([
-			Attribute\Min::class => new Validator\CheckMinCharCount(),
-			Attribute\Max::class => new Validator\CheckMaxCharCount(),
-		]);
+		// parent::__construct('name', $value);
 	}
 
-	public function minLengthOf(int $minChars): self
+	public function prefixWith(string $prefix): self
 	{
-		$this->attributes = $this->attributes->set(new Attribute\Min($minChars));
+		$this->removePrefix();
+
+		$this->prefix = $prefix;
+		$this->value = $this->prefix . $this->value;
 
 		return $this;
 	}
 
-	public function maxLengthOf(int $maxChars): self
+	public function removePrefix(): self
 	{
-		$this->attributes = $this->attributes->set(new Attribute\Max($maxChars));
+		$this->value = substr($this->value, strlen($this->prefix));
+		$this->prefix = '';
 
 		return $this;
-	}
-
-	public static function getSupportedAttributes(): array
-	{
-		return [
-			Attribute\Min::class,
-			Attribute\Max::class,
-		];
-	}
-
-	protected static function getTypeConstraintValidator(): Validator
-	{
-		return new class(self::TYPE_PATTERN) implements Validator {
-			public function __construct(private string $pattern) {}
-			public function validate(Attribute&Constraint $constraint, Field $field): bool
-			{
-				return is_string($field->value) && preg_match($this->pattern, $field->value) === 1;
-			}
-		};
 	}
 }
--- tests/Field/NameTest.php
+++ tests/Field/NameTest.php
@@ -3,81 +3,23 @@ declare(strict_types=1);
 
 namespace Meraki\Schema\Field;
 
-
 use Meraki\Schema\Field\Name;
-use Meraki\Schema\Attribute;
-use Meraki\Schema\Constraint;
-use Meraki\Schema\FieldTestCase;
-use PHPUnit\Framework\Attributes\{Test, CoversClass, DataProvider};
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\CoversClass;
 
+#[Group('field')]
 #[CoversClass(Name::class)]
-final class NameTest extends FieldTestCase
+final class NameTest extends TestCase
 {
 	#[Test]
-	public function it_exists(): void
+	public function it_sets_the_value_correctly(): void
 	{
-		$name = $this->createField();
+		$expectedName = 'field_name';
 
-		$this->assertInstanceOf(Name::class, $name);
-	}
+		$actualName = new Name($expectedName);
 
-	#[Test]
-	#[DataProvider('validNames')]
-	public function it_validates_valid_names(string $name): void
-	{
-		$field = $this->createField()
-			->input($name);
-
-		$this->assertTrue($field->validationResult->passed());
-		$this->assertValidationPassedForConstraint($field, Attribute\Type::class);
-	}
-
-	public static function validNames(): array
-	{
-		return [
-			'just first name' => ['John'],
-			'first and last name' => ['John Doe'],
-			'first, middle, and last name' => ['John Michael Doe'],
-			'with hyphen' => ['John-Michael Doe'],
-			'with apostrophe' => ['John O\'Doe'],
-			'with period' => ['John M. Doe'],
-			'with comma' => ['John, Doe'],
-			'with numerals' => ['John Doe III'],
-		];
-	}
-
-	public function createField(): Name
-	{
-		return new Name(new Attribute\Name('full_name'));
-	}
-
-	public function getExpectedType(): string
-	{
-		return 'name';
-	}
-
-	public function getValidValue(): mixed
-	{
-		return 'John Doe';
-	}
-
-	public function getInvalidValue(): mixed
-	{
-		return '123';
-	}
-
-	public function usesConstraints(): bool
-	{
-		return true;
-	}
-
-	public function createInvalidConstraint(): Constraint
-	{
-		return new Attribute\Max(2);
-	}
-
-	public function createValidConstraint(): Constraint
-	{
-		return new Attribute\Min(3);
+		$this->assertSame($expectedName, $actualName->value);
 	}
 }
