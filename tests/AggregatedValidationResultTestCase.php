e3ce24708fca03828f6a7a3037e0d8ea06605341
Nathan Bishop <nbish11@hotmail.com>
2025-04-22 16:13:37 +1000

    Make sure an aggregated validation result is still a validation result and passes validation result tests

--- tests/AggregatedValidationResultTestCase.php
+++ tests/AggregatedValidationResultTestCase.php
@@ -4,14 +4,22 @@ declare(strict_types=1);
 namespace Meraki\Schema;
 
 use Meraki\Schema\AggregatedValidationResult;
-use PHPUnit\Framework\TestCase;
+use Meraki\Schema\ValidationResultTestCase;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\CoversClass;
 
 #[Group('validation')]
 #[CoversClass(AggregatedValidationResult::class)]
-abstract class AggregatedValidationResultTestCase extends TestCase
+abstract class AggregatedValidationResultTestCase extends ValidationResultTestCase
 {
-	abstract public function createAggregatedValidationResult(): AggregatedValidationResult;
+	abstract public function createValidationResult(): AggregatedValidationResult;
+
+	#[Test]
+	public function it_is_an_aggregated_validation_result(): void
+	{
+		$result = $this->createValidationResult();
+
+		$this->assertInstanceOf(AggregatedValidationResult::class, $result);
+	}
 }
