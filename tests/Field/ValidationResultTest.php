736b2ed6fe3116a0dfdfe18ef1e10ac3c67e85e4
Nathan Bishop <nbish11@hotmail.com>
2025-04-22 16:10:06 +1000

    Schema field have their own validation result

---
+++ tests/Field/ValidationResultTest.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Schema\Field;
+
+use Meraki\Schema\AggregatedValidationResultTestCase;
+use Meraki\Schema\Field;
+use Meraki\Schema\Field\ValidationResult;
+use Meraki\Schema\AggregatedValidationResult;
+use Meraki\Schema\ValidationResult as ValidationResultInterface;
+use Meraki\Schema\ValidationStatus;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Group;
+
+#[Group('field')]
+#[Group('validation')]
+#[CoversClass(ValidationResult::class)]
+final class ValidationResultTest extends AggregatedValidationResultTestCase
+{
+	public function createValidationResult(): ValidationResult
+	{
+		return new ValidationResult($this->mockField());
+	}
+
+	#[Test]
+	public function it_can_retrieve_the_field_associated_with_result(): void
+	{
+		$field = $this->mockField();
+		$result = new ValidationResult($field);
+
+		$this->assertSame($field, $result->field);
+	}
+
+	#[Test]
+	public function it_is_in_the_pending_state_if_no_results(): void
+	{
+		$result = new ValidationResult($this->mockField());
+
+		$this->assertCount(0, $result);
+		$this->assertCount(0, $result->getPending());
+		$this->assertCount(0, $result->getPassed());
+		$this->assertCount(0, $result->getFailed());
+		$this->assertCount(0, $result->getSkipped());
+		$this->assertEquals(ValidationStatus::Pending, $result->status);
+	}
+
+	private function mockField(): Field
+	{
+		return $this->createMock(Field::class);
+	}
+}
