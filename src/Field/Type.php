21c32ea1738879746f8b0218ea7d7b408c2625de
Nathan Bishop <nbish11@hotmail.com>
2025-04-23 20:16:02 +1000

    Remove useless method

--- src/Field/Type.php
+++ src/Field/Type.php
@@ -3,16 +3,12 @@ declare(strict_types=1);
 
 namespace Meraki\Schema\Field;
 
-use Meraki\Schema\Validator\CheckType;
-
 /**
  * @property-read string $name
  */
 interface Type
 {
 	public function accepts(mixed $value): bool;
-
-	public function getValidator(): CheckType;
 
 	// public function isMissing(mixed $value): bool;
 
