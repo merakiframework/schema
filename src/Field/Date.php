cf574c899ded9250614dac6b9225f6466c7ed4b8
Nathan Bishop <nbish11@hotmail.com>
2025-05-01 16:21:15 +1000

    Convert Date type into a field

---
+++ src/Field/Date.php
@@ -0,0 +1,111 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Schema\Field;
+
+use Meraki\Schema\Field\Atomic as AtomicField;
+use Meraki\Schema\Property;
+use Brick\DateTime\DateTimeException;
+use Brick\DateTime\Period;
+use Brick\DateTime\LocalDate;
+
+final class Date extends AtomicField
+{
+	public LocalDate $from;
+	public LocalDate $until;
+	public Period $interval;
+
+	public function __construct(
+		Property\Name $name,
+		Property\Value $value = null,
+		Property\Value $defaultValue = null,
+		bool $optional = false,
+	) {
+		parent::__construct(new Property\Type('date'), $name, $value, $defaultValue, $optional);
+
+		$this->from = LocalDate::min();
+		$this->until = LocalDate::max();
+		$this->interval = Period::ofDays(1);
+	}
+
+	/**
+	 * This is inclusive of the date provided.
+	 */
+	public function from(string $date): self
+	{
+		$this->from = LocalDate::parse($date);
+
+		return $this;
+	}
+
+	/**
+	 * This is exclusive of the date provided.
+	 */
+	public function until(string $date): self
+	{
+		$this->until = LocalDate::parse($date);
+
+		return $this;
+	}
+
+	/**
+	 * This is inclusive of the date provided.
+	 */
+	public function to(string $date): self
+	{
+		$this->until = LocalDate::parse($date)->plusDays(1);
+
+		return $this;
+	}
+
+	public function atIntervalsOf(string $date): self
+	{
+		$this->interval = Period::parse($date);
+
+		return $this;
+	}
+
+	protected function cast(mixed $value): LocalDate
+	{
+		return LocalDate::parse($value);
+	}
+
+	protected function validateType(mixed $value): bool
+	{
+		if (!is_string($value)) {
+			return false;
+		}
+
+		try {
+			$date = $this->cast($value);
+			return true;
+		} catch (DateTimeException $e) {
+			return false;
+		}
+	}
+
+	protected function getConstraints(): array
+	{
+		return [
+			'from' => $this->validateFrom(...),
+			'until' => $this->validateUntil(...),
+			'interval' => $this->validateInterval(...),
+		];
+	}
+
+	private function validateFrom(mixed $value): bool
+	{
+		return $this->cast($value)->isAfterOrEqualTo($this->from);
+	}
+
+	private function validateUntil(mixed $value): bool
+	{
+		return $this->cast($value)->isBefore($this->until);
+	}
+
+	private function validateInterval(mixed $value): bool
+	{
+		$period = Period::between($this->from, $this->cast($value));
+		return $period->isEqualTo($this->interval) || $period->isZero();
+	}
+}
---
+++ tests/Field/DateTest.php
@@ -0,0 +1,249 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Schema\Field;
+
+use Meraki\Schema\Field\Date;
+use Meraki\Schema\Property\Name;
+use Meraki\Schema\FieldTestCase;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Group;
+
+#[Group('field')]
+#[CoversClass(Date::class)]
+final class DateTest extends FieldTestCase
+{
+	public function createField(): Date
+	{
+		return new Date(new Name('date'));
+	}
+
+	#[Test]
+	#[DataProvider('invalidDates')]
+	public function it_does_not_validate_invalid_dates(string $date): void
+	{
+		$field = $this->createField()->input($date);
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultFailed('type', $result);
+	}
+
+	#[Test]
+	public function it_validates_valid_dates(): void
+	{
+		$field = $this->createField()->input('2025-02-23');
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultPassed('type', $result);
+	}
+
+	#[Test]
+	public function it_validates_dates_with_a_default_value(): void
+	{
+		$field = $this->createField()
+			->prefill('2025-02-23')
+			->input(null);
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultPassed('type', $result);
+		$this->assertEquals('2025-02-23', $field->resolvedValue->unwrap());
+	}
+
+	#[Test]
+	public function it_validates_dates_with_a_default_value_and_a_value(): void
+	{
+		$field = $this->createField()
+			->prefill('2025-02-23')
+			->input('2025-02-24');
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultPassed('type', $result);
+		$this->assertEquals('2025-02-24', $field->resolvedValue->unwrap());
+	}
+
+	#[Test]
+	public function min_constraint_passes_when_input_is_same_as_min_date(): void
+	{
+		$field = $this->createField()
+			->from('2025-02-23')
+			->input('2025-02-23');
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultPassed('type', $result);
+		$this->assertConstraintValidationResultPassed('min', $result);
+	}
+
+	#[Test]
+	public function min_constraint_passes_when_input_is_past_min_date(): void
+	{
+		$field = $this->createField()
+			->from('2025-02-23')
+			->input('2025-02-24');
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultPassed('type', $result);
+		$this->assertConstraintValidationResultPassed('min', $result);
+	}
+
+	#[Test]
+	public function min_constraint_fails_when_input_is_before_min_date(): void
+	{
+		$field = $this->createField()
+			->from('2025-02-23')
+			->input('2025-02-22');
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultPassed('type', $result);
+		$this->assertConstraintValidationResultFailed('min', $result);
+	}
+
+	#[Test]
+	public function until_max_constraint_passes_when_input_is_before_max_date(): void
+	{
+		$field = $this->createField()
+			->until('2025-02-23')
+			->input('2025-02-22');
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultPassed('type', $result);
+		$this->assertConstraintValidationResultPassed('max', $result);
+	}
+
+	#[Test]
+	public function until_max_constraint_fails_when_input_is_at_max_date(): void
+	{
+		$field = $this->createField()
+			->until('2025-02-23')
+			->input('2025-02-23');
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultPassed('type', $result);
+		$this->assertConstraintValidationResultFailed('max', $result);
+	}
+
+	#[Test]
+	public function until_max_constraint_fails_when_input_is_after_max_date(): void
+	{
+		$field = $this->createField()
+			->until('2025-02-23')
+			->input('2025-02-24');
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultPassed('type', $result);
+		$this->assertConstraintValidationResultFailed('max', $result);
+	}
+
+	#[Test]
+	public function to_max_constraint_passes_when_input_is_before_max_date(): void
+	{
+		$field = $this->createField()
+			->to('2025-02-21')
+			->input('2025-02-22');
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultPassed('type', $result);
+		$this->assertConstraintValidationResultPassed('max', $result);
+	}
+
+	#[Test]
+	public function to_max_constraint_passes_when_input_is_at_max_date(): void
+	{
+		$field = $this->createField()
+			->to('2025-02-22')
+			->input('2025-02-22');
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultPassed('type', $result);
+		$this->assertConstraintValidationResultPassed('max', $result);
+	}
+
+	#[Test]
+	public function to_max_constraint_fails_when_input_past_max_date(): void
+	{
+		$field = $this->createField()
+			->to('2025-02-22')
+			->input('2025-02-23');
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultPassed('type', $result);
+		$this->assertConstraintValidationResultFailed('max', $result);
+	}
+
+	#[Test]
+	#[DataProvider('validIntervals')]
+	public function interval_constraint_passes_when_met(string $from, string $interval, string $value): void
+	{
+		$field = $this->createField()
+			->from($from)
+			->atIntervalsOf($interval)
+			->input($value);
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultPassed('type', $result);
+		$this->assertConstraintValidationResultPassed('interval', $result);
+	}
+
+	#[Test]
+	#[DataProvider('invalidIntervals')]
+	public function interval_constraint_fails_when_not_met(string $from, string $interval, string $value): void
+	{
+		$field = $this->createField()
+			->from($from)
+			->atIntervalsOf($interval)
+			->input($value);
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultPassed('type', $result);
+		$this->assertConstraintValidationResultFailed('interval', $result);
+	}
+
+	public static function invalidDates(): array
+	{
+		return [
+			'nothing given' => [''],
+			'date and time' => ['2025-02-23T12:34:56'],
+			'time' => ['12:34:56'],
+			'wrong format' => ['23-02-2025'],
+			'single digit month' => ['2025-2-23'],
+			'single digit day' => ['2025-02-3'],
+			'invalid month' => ['2025-13-23'],
+			'invalid day' => ['2025-02-32'],
+			'invalid leap year' => ['2025-02-29'],
+			'year too short' => ['25-02-23'],
+		];
+	}
+
+	public static function validIntervals(): array
+	{
+		return [
+			'same day' => ['2025-02-20', 'P1D', '2025-02-20'],
+			'the next day' => ['2025-02-20', 'P1D', '2025-02-21'],
+			'in a week' => ['2025-02-20', 'P7D', '2025-02-27'],
+		];
+	}
+
+	public static function invalidIntervals(): array
+	{
+		return [
+			'less than right amount of days' => ['2025-02-20', 'P7D', '2025-02-25'],
+			'more than right amount of days' => ['2025-02-20', 'P7D', '2025-03-07'],
+		];
+	}
+}
