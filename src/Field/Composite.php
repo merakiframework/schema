0baea270a076662074885a65c19b73958f24ddee
Nathan Bishop <nbish11@hotmail.com>
2025-05-03 12:07:50 +1000

    Provide a way to differentiate between different types of fields

---
+++ src/Field/Composite.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Schema\Field;
+
+use Meraki\Schema\Field;
+use Meraki\Schema\Property;
+
+/**
+ * @extends Field<null|array>
+ */
+abstract class Composite extends Field
+{
+	protected array $fields = [];
+
+	public function input($value): static
+	{
+		parent::input($value);
+
+		if ($value === null) {
+			foreach ($this->fields as $field) {
+				$field->input(null);
+			}
+
+			return $this;
+		}
+
+		foreach ($this->value->unwrap() as $fieldName => $fieldValue) {
+			if (isset($this->fields[$fieldName])) {
+				$this->fields[$fieldName]->input($fieldValue);
+			}
+		}
+
+		return $this;
+	}
+
+	protected function process($value): Property\Value
+	{
+		return new Property\Value($value);
+	}
+}
