85d3e62833c9234de43509198473b9807c82e83a
Nathan Bishop <nbish11@hotmail.com>
2025-05-03 12:10:49 +1000

    Fix method signature not matching abstract method

---
+++ src/Field/CreditCard.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Schema\Field;
+
+use Meraki\Schema\Field\Composite as CompositeField;
+use Meraki\Schema\Property;
+
+final class CreditCard extends CompositeField
+{
+	public function __construct(
+		Property\Name $name,
+		Property\Value $value = null,
+		Property\Value $defaultValue = null,
+		bool $optional = false,
+	) {
+		parent::__construct(new Property\Type('credit_card'), $name, $value, $defaultValue, $optional);
+	}
+
+	protected function cast(mixed $value): mixed
+	{
+		return $value;
+	}
+
+	protected function validateType(mixed $value): bool
+	{
+		return is_string($value);
+	}
+
+	protected function getConstraints(): array
+	{
+		return [];
+	}
+}
