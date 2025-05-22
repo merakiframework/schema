f71d71027a8c19485cb6d959cef245b5f558b854
Nathan Bishop <nbish11@hotmail.com>
2025-05-22 15:46:01 +1000

    Provide specific validation result for composite fields

---
+++ src/Field/CompositeValidationResult.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Schema\Field;
+
+use Meraki\Schema\Field\ValidationResult as FieldValidationResult;
+use Meraki\Schema\ValidationStatus;
+use Meraki\Schema\AggregatedValidationResult;
+
+/**
+ * @extends AggregatedValidationResult<FieldValidationResult>
+ */
+final class CompositeValidationResult extends AggregatedValidationResult
+{
+	public readonly ValidationStatus $status;
+
+	public function __construct(
+		public readonly Composite $composite,
+		FieldValidationResult ...$fieldResults
+	) {
+		parent::__construct(...$fieldResults);
+
+		$this->status = $this->calculateStatus();
+	}
+
+	private function calculateStatus(): ValidationStatus
+	{
+		if ($this->isEmpty() || $this->anyPending()) {
+			return ValidationStatus::Pending;
+		}
+
+		if ($this->allFailed()) {
+			return ValidationStatus::Failed;
+		}
+
+		return ValidationStatus::Passed;
+	}
+
+	public function get(string $fieldName): ?FieldValidationResult
+	{
+		foreach ($this->results as $result) {
+			if ((string)$result->field->name === $fieldName) {
+				return $result;
+			}
+		}
+
+		return null;
+	}
+
+	public function __clone(): void
+	{
+		$this->composite = clone $this->composite;
+	}
+}
