c489037a751a2011bfa9fa7e610214c2f6ab1c0f
Nathan Bishop <nbish11@hotmail.com>
2025-05-22 15:36:35 +1000

    Add tests to 'field' group

--- tests/Field/SetTest.php
+++ tests/Field/SetTest.php
@@ -3,12 +3,13 @@ declare(strict_types=1);
 
 namespace Meraki\Schema\Field;
 
-
 use Meraki\Schema\Field\Set;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Group;
 
+#[Group('field')]
 #[CoversClass(Set::class)]
 final class SetTest extends TestCase
 {
