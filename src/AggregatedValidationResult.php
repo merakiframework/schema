8e35842ae8c0d8cdc4d45cc16490c53ded6dbd67
Nathan Bishop <nbish11@hotmail.com>
2025-05-22 15:42:07 +1000

    Convert from interface to abstract class

--- src/AggregatedValidationResult.php
+++ src/AggregatedValidationResult.php
@@ -11,51 +11,170 @@ use Countable;
  * @template T of ValidationResult
  * @extends IteratorAggregate<int, T>
  */
-interface AggregatedValidationResult extends IteratorAggregate, Countable, ValidationResult
+abstract class AggregatedValidationResult implements IteratorAggregate, Countable, ValidationResult
 {
 	/**
-	 * @param T $result
+	 * @var list<T> $results
+	 * @readonly
 	 */
-	public function add(ValidationResult $result): static;
+	public array $results;
+
+	/**
+	 * @param T ...$results
+	 */
+	public function __construct(ValidationResult ...$results)
+	{
+		$this->results = $results;
+	}
 
 	/**
 	 * @param T $result
 	 */
-	public function remove(ValidationResult $result): static;
+	public function add(ValidationResult $result): static
+	{
+		$self = clone $this;
+		$self->results[] = $result;
+
+		return $self;
+	}
 
 	/**
 	 * @param T $result
 	 */
-	public function contains(ValidationResult $result): bool;
+	public function remove(ValidationResult $result): static
+	{
+		$self = clone $this;
+		$self->results = array_filter($this->results, fn(ValidationResult $r): bool => $r !== $result);
 
-	public function allPassed(): bool;
-	public function anyPassed(): bool;
-	public function allSkipped(): bool;
-	public function anySkipped(): bool;
-	public function allPending(): bool;
-	public function anyPending(): bool;
-	public function allFailed(): bool;
-	public function anyFailed(): bool;
-	public function getFailed(): static;
-	public function getPending(): static;
-	public function getPassed(): static;
-	public function getSkipped(): static;
+		return $self;
+	}
+
+	/**
+	 * @param T $result
+	 */
+	public function contains(ValidationResult $result): bool
+	{
+		return in_array($result, $this->results, true);
+	}
+
+	public function allPassed(): bool
+	{
+		return $this->count() === $this->getPassed()->count();
+	}
+
+	public function anyPassed(): bool
+	{
+		return $this->getPassed()->isNotEmpty();
+	}
+
+	public function allSkipped(): bool
+	{
+		return $this->count() === $this->getSkipped()->count();
+	}
+
+	public function anySkipped(): bool
+	{
+		return $this->getSkipped()->isNotEmpty();
+	}
+
+	public function allPending(): bool
+	{
+		return $this->count() === $this->getPending()->count();
+	}
+
+	public function anyPending(): bool
+	{
+		return $this->getPending()->isNotEmpty();
+	}
+
+	public function allFailed(): bool
+	{
+		return $this->count() === $this->getFailed()->count();
+	}
+	public function anyFailed(): bool
+	{
+		return $this->getFailed()->isNotEmpty();
+	}
+
+	public function getFailed(): static
+	{
+		return $this->filter(fn(ValidationResult $result): bool => $result->status === ValidationStatus::Failed);
+	}
+
+	public function getPending(): static
+	{
+		return $this->filter(fn(ValidationResult $result): bool => $result->status === ValidationStatus::Pending);
+	}
+
+	public function getPassed(): static
+	{
+		return $this->filter(fn(ValidationResult $result): bool => $result->status === ValidationStatus::Passed);
+	}
+
+	public function getSkipped(): static
+	{
+		return $this->filter(fn(ValidationResult $result): bool => $result->status === ValidationStatus::Skipped);
+	}
 
 	/**
 	 * @param callable(T): bool $predicate
 	 */
-	public function filter(callable $predicate): static;
-	public function isEmpty(): bool;
-	public function isNotEmpty(): bool;
+	public function filter(callable $predicate): static
+	{
+		$self = clone $this;
+		$self->results = array_filter($this->results, $predicate);
+
+		return $self;
+	}
+
+	public function isEmpty(): bool
+	{
+		return $this->count() === 0;
+	}
+
+	public function isNotEmpty(): bool
+	{
+		return $this->count() > 0;
+	}
 
 	/**
 	 * @return T|null
 	 */
-	public function getFirst(): ?ValidationResult;
+	public function getFirst(): ?ValidationResult
+	{
+		return $this->results[0] ?? null;
+	}
+
+	/**
+	 * @return T|null
+	 */
+	public function getLast(): ?ValidationResult
+	{
+		return $this->results[count($this->results) - 1] ?? null;
+	}
 
 	/**
 	 * @template A of AggregatedValidationResult<T>
 	 * @param A $other
 	 */
-	public function merge(self $other): static;
+	public function merge(self $other): static
+	{
+		$self = clone $this;
+		$self->results = array_merge($this->results, $other->results);
+
+		return $self;
+	}
+
+	public function count(): int
+	{
+		return count($this->results);
+	}
+
+	/**
+	 * @return \ArrayIterator<T>
+	 */
+	public function getIterator(): \ArrayIterator
+	{
+		return new \ArrayIterator($this->results);
+	}
 }
