228451586924e0a5425c0f6bc6da6219bb4d4b44
Nathan Bishop <nbish11@hotmail.com>
2025-05-23 21:13:10 +1000

    Make validation result return type more restrictive

--- src/Field/Atomic.php
+++ src/Field/Atomic.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 namespace Meraki\Schema\Field;
 
 use Meraki\Schema\Field;
+use Meraki\Schema\Field\ValidationResult;
 
 /**
  * @extends Field<mixed>
  */
 abstract class Atomic extends Field
 {
+	public function validate(): ValidationResult
+	{
+		return parent::validate();
+	}
 }
