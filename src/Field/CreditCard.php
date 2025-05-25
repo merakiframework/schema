f74bf4affe68e1d0edaa1702e9340831b898006d
Nathan Bishop <nbish11@hotmail.com>
2025-05-25 21:54:13 +1000

    Convert credit_card type into a field

--- src/Field/CreditCard.php
+++ src/Field/CreditCard.php
@@ -3,18 +3,67 @@ declare(strict_types=1);
 
 namespace Meraki\Schema\Field;
 
+use Brick\DateTime\TimeZone;
+use Brick\DateTime\ZonedDateTime;
+use Meraki\Schema\Field;
 use Meraki\Schema\Field\Composite as CompositeField;
 use Meraki\Schema\Property;
+use DateTimeImmutable;
 
+/**
+ * @property-read Field\Name $holder
+ * @property-read Field\Text $number
+ * @property-read Field\Date $expiry
+ * @property-read Field\Text $securityCode
+ */
 final class CreditCard extends CompositeField
 {
 	public function __construct(
 		Property\Name $name,
-		Property\Value $value = null,
-		Property\Value $defaultValue = null,
-		bool $optional = false,
 	) {
-		parent::__construct(new Property\Type('credit_card'), $name, $value, $defaultValue, $optional);
+		parent::__construct(
+			new Property\Type('credit_card', $this->validateType(...)),
+			$name,
+			$this->createHolderField(),
+			$this->createNumberField(),
+			$this->createExpiryField(),
+			$this->createSecurityCodeField(),
+		);
+
+		$holderName = (new Property\Name('holder'))->prefixWith($name)->__toString();
+		$numberName = (new Property\Name('number'))->prefixWith($name)->__toString();
+		$expiryName = (new Property\Name('expiry'))->prefixWith($name)->__toString();
+		$securityCodeName = (new Property\Name('security_code'))->prefixWith($name)->__toString();
+		$this->defaultValue = new Property\Value([
+			$holderName => null,
+			$numberName => null,
+			$expiryName => null,
+			$securityCodeName => null,
+		]);
+		$this->value = new Property\Value([
+			$holderName => null,
+			$numberName => null,
+			$expiryName => null,
+			$securityCodeName => null,
+		]);
+	}
+
+	/** @param array $value */
+	public function prefill($value): static
+	{
+		$value = $this->addDayToExpiry($value);
+		$value = $this->removeWhitespaceFromNumber($value);
+
+		return parent::prefill($value);
+	}
+
+	/** @param array $value */
+	public function input($value): static
+	{
+		$value = $this->addDayToExpiry($value);
+		$value = $this->removeWhitespaceFromNumber($value);
+
+		return parent::input($value);
 	}
 
 	protected function cast(mixed $value): mixed
@@ -22,13 +71,74 @@ final class CreditCard extends CompositeField
 		return $value;
 	}
 
-	protected function validateType(mixed $value): bool
-	{
-		return is_string($value);
-	}
-
 	protected function getConstraints(): array
 	{
 		return [];
+	}
+
+	private function removeWhitespaceFromNumber(array $value): array
+	{
+		$name = (string)(new Property\Name('number'))->prefixWith($this->name);
+
+		if (is_array($value) && isset($value[$name])) {
+			$number = $value[$name];
+
+			// Remove all whitespace from the number
+			if (is_string($number)) {
+				$value[$name] = preg_replace('/\s+/', '', $number);
+			}
+		}
+
+		return $value;
+	}
+
+	private function addDayToExpiry(array $value): array
+	{
+		$name = (string)(new Property\Name('expiry'))->prefixWith($this->name);
+
+		if (is_array($value) && isset($value[$name])) {
+			$expiry = $value[$name];
+
+			// Add the last day of the month to the expiry date
+			if (is_string($expiry) && preg_match('/^\d{4}-\d{2}$/', $expiry)) {
+				$expiryDate = DateTimeImmutable::createFromFormat('Y-m', $expiry);
+
+				if ($expiryDate !== false) {
+					$expiryDate = $expiryDate->modify('last day of this month');
+					$value[$name] = $expiryDate->format('Y-m-d');
+				}
+			}
+		}
+
+		return $value;
+	}
+
+	private function createHolderField(): Field\Name
+	{
+		return new Field\Name(new Property\Name('holder'));
+	}
+
+	private function createNumberField(): Field\Text
+	{
+		return (new Field\Text(new Property\Name('number')))
+			->minLengthOf(13)
+			->maxLengthOf(19)
+			->matches('/^\d+$/');
+	}
+
+	private function createExpiryField(): Field\Date
+	{
+		$now = ZonedDateTime::now(TimeZone::utc());
+
+		return (new Field\Date(new Property\Name('expiry')))
+			->from((string)$now->getDate());
+	}
+
+	private function createSecurityCodeField(): Field\Text
+	{
+		return (new Field\Text(new Property\Name('security_code')))
+			->minLengthOf(3)
+			->maxLengthOf(4)
+			->matches('/^\d+$/');
 	}
 }
---
+++ tests/Field/CreditCardTest.php
@@ -0,0 +1,275 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Schema\Field;
+
+use Meraki\Schema\Field;
+use Meraki\Schema\Field\CompositeTestCase;
+use Meraki\Schema\Field\CreditCard;
+use Meraki\Schema\Property\Name;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+#[Group('field')]
+#[CoversClass(CreditCard::class)]
+final class CreditCardTest extends CompositeTestCase
+{
+	public function createSubject(): CreditCard
+	{
+		return new CreditCard(new Name('credit_card'));
+	}
+
+	public function createField(): CreditCard
+	{
+		return $this->createSubject();
+	}
+
+	#[Test]
+	public function subfields_are_created(): void
+	{
+		$field = $this->createSubject();
+
+		$this->assertInstanceOf(Field\Name::class, $field->holder);
+		$this->assertInstanceOf(Field\Text::class, $field->number);
+		$this->assertInstanceOf(Field\Date::class, $field->expiry);
+		$this->assertInstanceOf(Field\Text::class, $field->securityCode);
+	}
+
+	#[Test]
+	public function subfields_have_correct_names(): void
+	{
+		$field = $this->createSubject();
+
+		$this->assertEquals('credit_card.holder', (string)$field->holder->name);
+		$this->assertEquals('credit_card.number', (string)$field->number->name);
+		$this->assertEquals('credit_card.expiry', (string)$field->expiry->name);
+		$this->assertEquals('credit_card.security_code', (string)$field->securityCode->name);
+	}
+
+	#[Test]
+	#[DataProvider('validCreditCards')]
+	public function it_validates_valid_credit_cards(string $holder, string $number, string $expiry, string $securityCode): void
+	{
+		$field = $this->createSubject()->input([
+			'credit_card.holder' => $holder,
+			'credit_card.number' => $number,
+			'credit_card.expiry' => $expiry,
+			'credit_card.security_code' => $securityCode,
+		]);
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultPassedForField('credit_card.holder', 'type', $result);
+
+		$this->assertConstraintValidationResultPassedForField('credit_card.number', 'type', $result);
+		$this->assertConstraintValidationResultPassedForField('credit_card.number', 'min', $result);
+		$this->assertConstraintValidationResultPassedForField('credit_card.number', 'max', $result);
+
+		$this->assertConstraintValidationResultPassedForField('credit_card.expiry', 'type', $result);
+
+		$this->assertConstraintValidationResultPassedForField('credit_card.security_code', 'type', $result);
+		$this->assertConstraintValidationResultPassedForField('credit_card.security_code', 'min', $result);
+		$this->assertConstraintValidationResultPassedForField('credit_card.security_code', 'max', $result);
+	}
+
+	public static function validCreditCards(): array
+	{
+		// https://www.creditscardgenerator.com/
+		return [
+			'visa (13 digits)' => ['Matthew James', '4267 7724 0310 5', '2026-04', '242'],
+			'visa (16 digits)' => ['Kenneth Miller MD', '4014 1828 2909 8805', '2027-10', '936'],
+			'visa (19 digits)' => ['Ryan Hall', '4958 5581 8834 3371 581', '2027-12', '449'],
+			'mastercard' => ['Brandon Ramirez', '2720 0792 6056 8243', '2026-12', '594'],
+			'amex' => ['Elizabeth Cooper', '3407 769523 04418', '2029-04', '9635'],
+			'discover (16 digits)' => ['Mark Lopez', '6466 5921 0488 9007', '2027-02', '960'],
+			'discover (19 digits)' => ['Skylar Reed', '6469 8745 3650 4031 159', '2028-03', '289'],
+			'diners club (14 digits)' => ['William Morgan', '3038 4195 6843 35', '2028-09', '038'],
+			'diners club (16 digits)' => ['Audrey Price III', '3882 2342 3459 5627', '2028-08', '803'],
+			'diners club (19 digits)' => ['William Murphy Jr.', '3049 1782 8122 4673 771', '2030-05', '067'],
+			'jcb (16 digits)' => ['Mark Williams PhD', '3529 7754 7388 9646', '2027-07', '446'],
+			'jcb (19 digits)' => ['Victoria Smith', '3579 7188 3488 3186 615', '2026-10', '198'],
+		];
+	}
+
+	#[Test]
+	public function it_fails_if_holder_is_not_provided(): void
+	{
+		$field = $this->createSubject()->input([
+			'credit_card.holder' => '',
+			'credit_card.number' => '4014 1828 2909 8805',
+			'credit_card.expiry' => '2027-10',
+			'credit_card.security_code' => '936',
+		]);
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultFailedForField('credit_card.holder', 'type', $result);
+	}
+
+	#[Test]
+	public function it_fails_if_number_is_not_provided(): void
+	{
+		$field = $this->createSubject()->input([
+			'credit_card.holder' => 'Kenneth Miller MD',
+			'credit_card.number' => '',
+			'credit_card.expiry' => '2027-10',
+			'credit_card.security_code' => '936',
+		]);
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultFailedForField('credit_card.number', 'min', $result);
+	}
+
+	#[Test]
+	public function it_fails_if_number_is_too_short(): void
+	{
+		$field = $this->createSubject()->input([
+			'credit_card.holder' => 'Kenneth Miller MD',
+			'credit_card.number' => '4014 1828 2909',
+			'credit_card.expiry' => '2027-10',
+			'credit_card.security_code' => '936',
+		]);
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultFailedForField('credit_card.number', 'min', $result);
+	}
+
+	#[Test]
+	public function it_fails_if_number_is_too_long(): void
+	{
+		$field = $this->createSubject()->input([
+			'credit_card.holder' => 'Kenneth Miller MD',
+			'credit_card.number' => '4958 5581 8834 3371 5812',
+			'credit_card.expiry' => '2027-10',
+			'credit_card.security_code' => '936',
+		]);
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultFailedForField('credit_card.number', 'max', $result);
+	}
+
+	#[Test]
+	public function it_fails_if_security_code_is_not_provided(): void
+	{
+		$field = $this->createSubject()->input([
+			'credit_card.holder' => 'Kenneth Miller MD',
+			'credit_card.number' => '4014 1828 2909 8805',
+			'credit_card.expiry' => '2027-10',
+			'credit_card.security_code' => '',
+		]);
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultFailedForField('credit_card.security_code', 'min', $result);
+	}
+
+	#[Test]
+	public function it_fails_if_security_code_is_too_short(): void
+	{
+		$field = $this->createSubject()->input([
+			'credit_card.holder' => 'Kenneth Miller MD',
+			'credit_card.number' => '4014 1828 2909 8805',
+			'credit_card.expiry' => '2027-10',
+			'credit_card.security_code' => '93',
+		]);
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultFailedForField('credit_card.security_code', 'min', $result);
+	}
+
+	#[Test]
+	public function it_fails_if_security_code_is_too_long(): void
+	{
+		$field = $this->createSubject()->input([
+			'credit_card.holder' => 'Kenneth Miller MD',
+			'credit_card.number' => '4014 1828 2909 8805',
+			'credit_card.expiry' => '2027-10',
+			'credit_card.security_code' => '93675',
+		]);
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultFailedForField('credit_card.security_code', 'max', $result);
+	}
+
+	#[Test]
+	public function it_has_no_value_by_default(): void
+	{
+		$field = $this->createSubject();
+
+		$this->assertEquals(
+			['credit_card.holder' => null, 'credit_card.number' => null, 'credit_card.expiry' => null, 'credit_card.security_code' => null],
+			$field->value->unwrap()
+		);
+		$this->assertEquals(null, $field->holder->value->unwrap());
+		$this->assertEquals(null, $field->number->value->unwrap());
+		$this->assertEquals(null, $field->expiry->value->unwrap());
+		$this->assertEquals(null, $field->securityCode->value->unwrap());
+	}
+
+	#[Test]
+	public function it_has_no_default_value_by_default(): void
+	{
+		$field = $this->createSubject();
+
+		$this->assertEquals(
+			['credit_card.holder' => null, 'credit_card.number' => null, 'credit_card.expiry' => null, 'credit_card.security_code' => null],
+			$field->value->unwrap()
+		);
+		$this->assertEquals(null, $field->holder->defaultValue->unwrap());
+		$this->assertEquals(null, $field->number->defaultValue->unwrap());
+		$this->assertEquals(null, $field->expiry->defaultValue->unwrap());
+		$this->assertEquals(null, $field->securityCode->defaultValue->unwrap());
+	}
+
+	#[Test]
+	public function it_fails_if_expiry_is_not_provided(): void
+	{
+		$field = $this->createSubject()->input([
+			'credit_card.holder' => 'Kenneth Miller MD',
+			'credit_card.number' => '4014 1828 2909 8805',
+			'credit_card.expiry' => '',
+			'credit_card.security_code' => '936',
+		]);
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultFailedForField('credit_card.expiry', 'type', $result);
+	}
+
+	#[Test]
+	public function it_fails_if_expiry_is_in_the_past(): void
+	{
+		$field = $this->createSubject()->input([
+			'credit_card.holder' => 'Kenneth Miller MD',
+			'credit_card.number' => '4014 1828 2909 8805',
+			'credit_card.expiry' => '2020-01',
+			'credit_card.security_code' => '936',
+		]);
+
+		$result = $field->validate();
+
+		$this->assertConstraintValidationResultFailedForField('credit_card.expiry', 'from', $result);
+	}
+
+	#[Test]
+	public function end_of_month_day_is_automatically_added_to_end_of_expiry_date(): void
+	{
+		$field = $this->createSubject()->input([
+			'credit_card.holder' => 'Kenneth Miller MD',
+			'credit_card.number' => '4014 1828 2909 8805',
+			'credit_card.expiry' => '2029-07',
+			'credit_card.security_code' => '936',
+		]);
+
+		$result = $field->validate();
+
+		$this->assertEquals('2029-07-31', $field->expiry->resolvedValue->unwrap());
+	}
+}
