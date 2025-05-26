8b04596a978521067115dee8776f14eeaea6482d
Nathan Bishop <nbish11@hotmail.com>
2025-05-26 14:05:22 +1000

    Convert url type into a field (and rename to uri)

---
+++ src/Field/Uri.php
@@ -0,0 +1,94 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Schema\Field;
+
+use Meraki\Schema\Field\Atomic as AtomicField;
+use Meraki\Schema\Property;
+use InvalidArgumentException;
+
+final class Uri extends AtomicField
+{
+	private const PATTERN = '~^
+		(?:([^:/\?\#]+):)?	# scheme
+		(?://([^/\?\#]*))?	# authority
+		([^\?\#]*)			# path
+		(?:\?([^\#]*))?		# query
+		(?:\#(.*))?			# fragment
+	$~x';
+
+	public int $min = 0;
+
+	public int $max = PHP_INT_MAX;
+
+	public function __construct(
+		Property\Name $name,
+		Property\Value $value = null,
+		Property\Value $defaultValue = null,
+		bool $optional = false,
+	) {
+		parent::__construct(new Property\Type('uri', $this->validateType(...)), $name, $value, $defaultValue, $optional);
+	}
+
+	public function minLengthOf(int $minChars): self
+	{
+		if ($minChars < 0) {
+			throw new InvalidArgumentException('Minimum length must be a positive integer.');
+		}
+
+		if ($minChars > $this->max) {
+			throw new InvalidArgumentException('Minimum length cannot be greater than maximum length.');
+		}
+
+		$this->min = $minChars;
+
+		return $this;
+	}
+
+	public function maxLengthOf(int $maxChars): self
+	{
+		if ($maxChars < 0) {
+			throw new InvalidArgumentException('Maximum length must be a positive integer.');
+		}
+
+		if ($maxChars < $this->min) {
+			throw new InvalidArgumentException('Maximum length cannot be less than minimum length.');
+		}
+
+		if ($maxChars > PHP_INT_MAX) {
+			throw new InvalidArgumentException('Maximum length cannot exceed PHP_INT_MAX.');
+		}
+
+		$this->max = $maxChars;
+
+		return $this;
+	}
+
+	protected function cast(mixed $value): string
+	{
+		return $value;
+	}
+
+	protected function validateType(mixed $value): bool
+	{
+		return is_string($value) && preg_match(self::PATTERN, $value) === 1;
+	}
+
+	protected function getConstraints(): array
+	{
+		return [
+			'min' => $this->validateMin(...),
+			'max' => $this->validateMax(...),
+		];
+	}
+
+	private function validateMin(mixed $value): bool
+	{
+		return mb_strlen($value) >= $this->min;
+	}
+
+	private function validateMax(mixed $value): bool
+	{
+		return mb_strlen($value) <= $this->max;
+	}
+}
---
+++ tests/Field/UriTest.php
@@ -0,0 +1,142 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Schema\Field;
+
+use Meraki\Schema\Field\Uri;
+use Meraki\Schema\Property;
+use Meraki\Schema\FieldTestCase;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+#[Group('field')]
+#[CoversClass(Uri::class)]
+final class UriTest extends FieldTestCase
+{
+	public function createSubject(): Uri
+	{
+		return new Uri(new Property\Name('test'));
+	}
+
+	public function createField(): Uri
+	{
+		return $this->createSubject();
+	}
+
+	#[Test]
+	#[DataProvider('validAbsoluteUris')]
+	public function it_validates_valid_absolute_urls(string $uri): void
+	{
+		$sut = $this->createSubject()->input($uri);
+
+		$result = $sut->validate();
+
+		$this->assertConstraintValidationResultPassed('type', $result);
+	}
+
+	#[Test]
+	#[DataProvider('invalidAbsoluteUris')]
+	public function it_does_not_validate_invalid_absolute_urls(mixed $uri): void
+	{
+		$sut = $this->createSubject()->input($uri);
+
+		$result = $sut->validate();
+
+		$this->assertConstraintValidationResultFailed('type', $result);
+	}
+
+	#[Test]
+	public function min_constraint_passes_when_met(): void
+	{
+		$sut = $this->createSubject()
+			->input('https://example.com')
+			->minLengthOf(12);
+
+		$result = $sut->validate();
+
+		$this->assertConstraintValidationResultPassed('min', $result);
+	}
+
+	#[Test]
+	public function min_constraint_fails_when_not_met(): void
+	{
+		$sut = $this->createSubject()
+			->input('https://example.com')
+			->minLengthOf(30);
+
+		$result = $sut->validate();
+
+		$this->assertConstraintValidationResultFailed('min', $result);
+	}
+
+	#[Test]
+	public function max_constraint_passes_when_met(): void
+	{
+		$sut = $this->createSubject()
+			->input('https://example.com')
+			->maxLengthOf(20);
+
+		$result = $sut->validate();
+
+		$this->assertConstraintValidationResultPassed('max', $result);
+	}
+
+	#[Test]
+	public function max_constraint_fails_when_not_met(): void
+	{
+		$sut = $this->createSubject()
+			->input('https://example.com')
+			->maxLengthOf(12);
+
+		$result = $sut->validate();
+
+		$this->assertConstraintValidationResultFailed('max', $result);
+	}
+
+	public static function validAbsoluteUris(): array
+	{
+		return [
+			'just the domain' => ['https://example.com'],
+			'with port' => ['https://example.com:8080'],
+			'with path' => ['https://example.com/path/to/resource'],
+			'with query string' => ['https://example.com?query=string'],
+			'with fragment' => ['https://example.com#fragment'],
+			'with query string and fragment' => ['https://example.com?query=string#fragment'],
+			'with port, path, query string, and fragment' => ['https://example.com:8080/path/to/resource?query=string#fragment'],
+			'with user info (username only)' => ['https://user@example.com'],
+			'with user info (username and password)' => ['https://user:abc123@example.com'],
+		];
+	}
+
+	public static function invalidAbsoluteUris(): array
+	{
+		return [
+			// 'no scheme' => ['example.com'],
+			// 'no authority' => ['https:/path/to/resource'],
+			// 'no scheme, authority, query string, or fragment' => [''],
+			'no scheme, authority, path, query string, or fragment (null)' => [null],
+			'no scheme, authority, path, query string, or fragment (boolean)' => [false],
+			'no scheme, authority, path, query string, or fragment (signed integer)' => [0],
+			'no scheme, authority, path, query string, or fragment (array)' => [[]],
+			'no scheme, authority, path, query string, or fragment (object)' => [new \stdClass()],
+		];
+	}
+
+	#[Test]
+	public function it_has_no_value_by_default(): void
+	{
+		$sut = $this->createSubject();
+
+		$this->assertNull($sut->value->unwrap());
+	}
+
+	#[Test]
+	public function it_has_no_default_value_by_default(): void
+	{
+		$sut = $this->createSubject();
+
+		$this->assertNull($sut->defaultValue->unwrap());
+	}
+}
