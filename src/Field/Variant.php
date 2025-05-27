fad988be59e908e18e5b8c2579cad9a321b3a771
Nathan Bishop <nbish11@hotmail.com>
2025-05-27 18:16:55 +1000

    Add a 'variant' field type

---
+++ src/Field/Variant.php
@@ -0,0 +1,150 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Schema\Field;
+
+use Meraki\Schema\AggregatedValidationResult;
+use Meraki\Schema\Field;
+use Meraki\Schema\Field\ValidationResult;
+use Meraki\Schema\ValidationStatus;
+use Meraki\Schema\Field\Atomic as AtomicField;
+use Meraki\Schema\Property;
+use InvalidArgumentException;
+
+/**
+ * A variant field is a field that can have multiple types. It is used to represent a value that can be one of several different types.
+ * For example, a variant field can be used to represent a password type and a passphrase type.
+ * A variant field can only contain atomic fields, which are fields that have a single value.
+ * A variant field cannot use the same field type more than once.
+ * Each field in a variant must have a unique name, and the names are prefixed with the variant's name.
+ * The order that fields are added is the order that they are validated.
+ * The first field that matches the value is the one that is used. (e.g. if a value can match a password and a passphrase field, but
+ * the passphrase field was added first, then the passphrase field result is returned.)
+ *
+ * @template AcceptedType of mixed
+ */
+final class Variant extends Field
+{
+	public Field\Set $fields;
+
+	public ?AtomicField $matchedField = null;
+
+	public function __construct(
+		Property\Name $name,
+		AtomicField ...$fields
+	) {
+		parent::__construct(new Property\Type('variant', $this->validateType(...)), $name);
+
+		$this->fields = new Field\Set(...$fields);
+
+		if ($this->fields->containsDuplicateFieldTypes()) {
+			throw new InvalidArgumentException('Variant fields cannot contain duplicate field types.');
+		}
+
+		$this->rename($name);
+	}
+
+	public function rename(Property\Name $name): static
+	{
+		$this->name = $name;
+		$this->fields->prefixNamesWith($name);
+
+		return $this;
+	}
+
+	/** @param AcceptedType $value */
+	public function prefill($value): static
+	{
+		parent::prefill($value);
+
+		foreach ($this->fields as $field) {
+			try {
+				$field->prefill($value);
+			} catch (InvalidArgumentException $e) {
+				continue;
+			}
+		}
+
+		return $this;
+	}
+
+	/** @param AcceptedType $value */
+	public function input($value): static
+	{
+		parent::input($value);
+
+		foreach ($this->fields as $field) {
+			$field->input($value);
+		}
+
+		return $this;
+	}
+
+	public function validate(): AggregatedValidationResult
+	{
+		$value = $this->resolvedValue;
+
+		// if the field is optional and no value is provided, skip all constraints
+		if ($this->optional && $value->notProvided()) {
+			return new ValidationResult($this, ConstraintValidationResult::skip('type'));
+		}
+
+		// if the field is not optional and no value provided, return a validation error
+		if ($value->notProvided()) {
+			return new ValidationResult($this, ConstraintValidationResult::fail('type'));
+		}
+
+		$typeIsValid = ($this->type->validator)($value->unwrap());
+		$fieldResults = [];
+
+		if ($typeIsValid) {
+			foreach ($this->fields as $field) {
+				$result = $field->validate();
+
+				if ($result->status === ValidationStatus::Passed) {
+					$this->matchedField = $field;
+					$this->resolvedValue = $field->resolvedValue;
+					return $result;
+				}
+
+				$fieldResults[] = $result;
+			}
+		}
+
+		return new CompositeValidationResult($this, ...$fieldResults);
+	}
+
+	public function getConstraints(): array
+	{
+		return [];
+	}
+
+	public function __isset(string $name): bool
+	{
+		$name = self::camelCaseToSnakeCase($name);
+
+		return $this->fields->findByName($this->name->__toString() . $this->name::PREFIX_SEPARATOR . $name) !== null;
+	}
+
+	public function __get($name): Field
+	{
+		$name = self::camelCaseToSnakeCase($name);
+		$field = $this->fields->findByName($this->name->__toString() . $this->name::PREFIX_SEPARATOR . $name);
+
+		if ($field) {
+			return $field;
+		}
+
+		throw new InvalidArgumentException("Field '$name' does not exist.");
+	}
+
+	protected function validateType(mixed $value): bool
+	{
+		return true;
+	}
+
+	private static function camelCaseToSnakeCase(string $input): string
+	{
+		return strtolower(preg_replace('/[A-Z]/', '_$0', lcfirst($input)));
+	}
+}
---
+++ tests/Field/VariantTest.php
@@ -0,0 +1,182 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Schema\Field;
+
+use Meraki\Schema\Field;
+use Meraki\Schema\Property;
+use Meraki\Schema\Field\Variant as VariantField;
+use Meraki\Schema\FieldTestCase;
+use Meraki\Schema\ValidationStatus;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\DataProvider;
+use InvalidArgumentException;
+
+#[Group('field')]
+#[CoversClass(VariantField::class)]
+final class VariantTest extends FieldTestCase
+{
+	public function createSubject(): VariantField
+	{
+		return new VariantField(
+			new Property\Name('secret'),
+			new Field\Passphrase(new Property\Name('passphrase')),
+			new Field\Password(new Property\Name('password')),
+		);
+	}
+
+	public function createField(): VariantField
+	{
+		return $this->createSubject();
+	}
+
+	#[Test]
+	public function it_has_a_name(): void
+	{
+		$sut = $this->createSubject();
+
+		$this->assertInstanceOf(Property\Name::class, $sut->name);
+		$this->assertEquals('secret', (string) $sut->name);
+	}
+
+	#[Test]
+	public function it_has_a_type(): void
+	{
+		$sut = $this->createSubject();
+
+		$this->assertInstanceOf(Property\Type::class, $sut->type);
+		$this->assertEquals('variant', (string) $sut->type);
+	}
+
+	#[Test]
+	public function it_prefixes_field_names_correctly(): void
+	{
+		$sut = $this->createSubject();
+
+		$this->assertEquals('secret.passphrase', (string) $sut->passphrase->name);
+		$this->assertEquals('secret.password', (string) $sut->password->name);
+	}
+
+	#[Test]
+	public function it_has_no_default_value_by_default(): void
+	{
+		$sut = $this->createSubject();
+
+		$this->assertNull($sut->defaultValue->unwrap());
+	}
+
+	#[Test]
+	public function it_has_no_value_by_default(): void
+	{
+		$sut = $this->createSubject();
+
+		$this->assertNull($sut->value->unwrap());
+	}
+
+	#[Test]
+	public function it_throws_when_duplicate_field_types_are_added(): void
+	{
+		$this->expectException(InvalidArgumentException::class);
+		$this->expectExceptionMessage("Variant fields cannot contain duplicate field types.");
+
+		$sut = new VariantField(
+			new Property\Name('secret'),
+			new Field\Passphrase(new Property\Name('passphrase1')),
+			new Field\Passphrase(new Property\Name('passphrase2')),
+		);
+	}
+
+	#[Test]
+	public function it_returns_failed_validation_when_no_value_is_provided_and_field_is_required(): void
+	{
+		$sut = $this->createSubject();
+
+		$result = $sut->validate();
+
+		$this->assertConstraintValidationResultFailed('type', $result);
+	}
+
+	#[Test]
+	public function it_returns_skipped_validation_when_no_value_is_provided_and_field_is_optional(): void
+	{
+		$sut = $this->createSubject()->makeOptional();
+
+		$result = $sut->validate();
+
+		$this->assertConstraintValidationResultSkipped('type', $result);
+	}
+
+	#[Test]
+	public function it_uses_the_first_matching_field_for_validation(): void
+	{
+		$sut = $this->createSubject()
+			->input('correct horse battery staple'); // valid passphrase
+
+		$result = $sut->validate();
+
+		$this->assertEquals(ValidationStatus::Passed, $result->status);
+		$this->assertInstanceOf(Field\Passphrase::class, $result->field);
+		$this->assertInstanceOf(Field\Passphrase::class, $sut->matchedField);
+	}
+
+	#[Test]
+	public function it_uses_the_second_matching_field_if_the_first_does_not_match(): void
+	{
+		$sut = $this->createSubject()
+			->input('password');
+
+		$result = $sut->validate();
+
+		$this->assertEquals(ValidationStatus::Passed, $result->status);
+		$this->assertInstanceOf(Field\Password::class, $result->field);
+		$this->assertInstanceOf(Field\Password::class, $sut->matchedField);
+	}
+
+	#[Test]
+	public function it_fails_validation_if_no_fields_match(): void
+	{
+		$sut = new VariantField(
+			new Property\Name('secret'),
+			Field\Passphrase::paranoid(new Property\Name('passphrase')),
+			Field\Password::strong(new Property\Name('password')),
+		);
+		$sut->input('x');
+
+		$result = $sut->validate();
+
+		$this->assertEquals(ValidationStatus::Failed, $result->status);
+		$this->assertTrue($result->allFailed());
+	}
+
+	#[Test]
+	public function it_updates_its_resolved_value_to_the_matched_field_value(): void
+	{
+		$input = 'correct horse battery staple'; // valid passphrase
+
+		$sut = $this->createSubject()->input($input);
+
+		$this->assertEquals($input, $sut->resolvedValue->unwrap());
+	}
+
+	#[Test]
+	public function it_prefills_value_to_fields_correctly(): void
+	{
+		$value = 'correct horse battery staple';
+		$sut = $this->createSubject()->prefill($value);
+
+		$this->assertEquals($value, $sut->passphrase->defaultValue->unwrap());
+		$this->assertEquals($value, $sut->password->defaultValue->unwrap());
+	}
+
+	#[Test]
+	public function it_inputs_value_to_fields_correctly(): void
+	{
+		$value = 'correct horse battery staple';
+		$sut = $this->createSubject()->input($value);
+
+		$this->assertEquals($value, $sut->passphrase->value->unwrap());
+		$this->assertEquals($value, $sut->password->value->unwrap());
+	}
+}
