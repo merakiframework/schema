ee2c16501b1a9ab41a6e496738a19defac86d126
Nathan Bishop <nbish11@hotmail.com>
2025-06-17 10:35:13 +1000

    Update code to match new API

--- examples/serialize.php
+++ examples/serialize.php
@@ -2,48 +2,34 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Meraki\Schema\Rule\Outcome;
+use Meraki\Schema\Field;
 use Meraki\Schema\Rule;
+use Meraki\Schema\Rule\Builder;
+use Meraki\Schema\Rule\Outcome;
 use Meraki\Schema\Rule\Condition;
-use Meraki\Schema\Rule\OutcomeGroup;
 
-$schema = new Meraki\Schema\SchemaFacade('contact_form');
+$schema = new Meraki\Schema\Facade('contact_us');
+$schema->addNameField('name');
+$schema->addEnumField('contact_method', ['email', 'phone']);
+$schema->addEmailAddressField('email_address');
+$schema->addPhoneNumberField('phone_number');
+$schema->addTextField('message', fn(Field\Text $field): Field\Text => $field->minLengthOf(10)->maxLengthOf(500));
 
-$name = $schema->addNameField('name')
-	->require();
+// manipulating rules using the fluent interface
+$schema->whenAllMatch(fn(Builder $r): Builder =>
+	$r->whenEquals('#/fields/contact_method/value', 'email')
+		->thenMakeOptional('#/fields/phone_number')
+);
 
-$contactMethod = $schema->addEnumField('contact_method', ['email', 'phone'])
-	->require();
-
-$message = $schema->addTextField('message')
-	->require()
-	->minLengthOf(10)
-	->maxLengthOf(500);
-
-$emailAddress = $schema->addEmailAddressField('email_address');
-
-$phoneNumber = $schema->addPhoneNumberField('phone_number');
-
-
+// manipulating rules directly
 $schema->addRule(
-	Rule::matchAny()
-		->when(
-			Condition::matchAll(
-				Condition::create('#/field_1/value', 'equals', 'value1'),
-				Condition::create('#/field_2/value', 'equals', 'value2'),
-				Condition::matchAll(
-					Condition::create('#/field_3/value', 'equals', 'value3'),
-					Condition::create('#/field_4/value', 'notEquals', 'value4'),
-				),
-			),
-			Condition::matchNone(
-				Condition::create('#/contact_method/value', 'equals', 'phone'),
-			)
-		)
-		->then(
-			Outcome::require('#/email_address'),
-			Outcome::require('#/phone_number'),
-		)
+	Rule::when(new Condition\AllOf(
+		new Condition\Equals('#/fields/contact_method/value', 'phone')
+		// @eventually: new Scope('#/fields/contact_method/value')->equals('phone')
+	))->then(
+		new Outcome\MakeOptional('#/fields/email_address')
+		// @eventually: new Scope('#/fields/email_address')->makeOptional()
+	)
 );
 
 echo '<pre>';
