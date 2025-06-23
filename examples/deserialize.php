939881305034efcaa4f0f100677b5ca5346db30d
Nathan Bishop <nbish11@hotmail.com>
2025-06-23 17:06:11 +1000

    Add example of traversing schema

--- examples/deserialize.php
+++ examples/deserialize.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 echo '<pre>';
 
 $schema = Meraki\Schema\Facade::deserialize(__DIR__ . '/schema.json');
-var_dump($schema);
+
+var_dump($schema->traverse(new Meraki\Schema\Scope('#/fields/email_address/value')));
 
 echo '</pre>';
