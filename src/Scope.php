d32efe678aec059e72fbdfd140c9b8bc680533e2
Nathan Bishop <nbish11@hotmail.com>
2025-06-23 17:14:06 +1000

    Make scope more useful when working with scope paths

--- src/Scope.php
+++ src/Scope.php
@@ -1,136 +1,143 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Meraki\Schema;
 
-use Meraki\Schema\SchemaFacade;
+use Countable;
+use Iterator;
+use OutOfBoundsException;
+use Stringable;
 
-class Scope
+final class Scope implements Stringable, Countable, Iterator
 {
-	public function __construct(
-		private string $path
-	) {
-		// Validate the path to ensure it's a proper reference format
-		if (!preg_match('/^#\/|^\//', $this->path)) {
-			throw new \InvalidArgumentException('Invalid reference: ' . $this->path);
+	public readonly string $path;
+
+	private int $position;
+
+	/** @var string[] */
+	public readonly array $segments;	// always normalised to snake_case
+
+	/** @var string[] */
+	private readonly array $segmentsAsCamelCase;
+
+	public function __construct(string $path, int $position = 0)
+	{
+		$normalizedPath = strtolower(rtrim($path, '/'));
+
+		if (!preg_match('/^#\//', $normalizedPath)) {
+			throw new \InvalidArgumentException("Invalid path '$path'. Must start with '#/'.");
 		}
+
+		$this->path = $normalizedPath;
+		$this->position = $position;
+		$this->segments = explode('/', substr($normalizedPath, 2)); // remove leading '#/'
+		$this->segmentsAsCamelCase = array_map($this->snakeCaseToCamelCase(...), $this->segments);
+
+		$this->assertPositionInBounds();
 	}
 
-	public function getPath(): string
+	private function snakeCaseToCamelCase(string $segment): string
+	{
+		return lcfirst(str_replace('_', '', ucwords($segment, '_')));
+	}
+
+	public function get(int $index): string
+	{
+		$this->assertPositionInBounds($index);
+
+		return $this->segments[$index];
+	}
+
+	/**
+	 * returns the segments as snake case
+	 */
+	public function current(): ?string
+	{
+		return $this->currentAsSnakeCase();
+	}
+
+	public function key(): mixed
+	{
+		return $this->position;
+	}
+
+	public function next(): void
+	{
+		$this->position++;
+	}
+
+	public function rewind(): void
+	{
+		$this->position = 0;
+	}
+
+	public function isAbsolute(): bool
+	{
+		return str_starts_with($this->path, '#/');
+	}
+
+	public function isRoot(): bool
+	{
+		return $this->path === '#/';
+	}
+
+	public function currentAsCamelCase(): ?string
+	{
+		return $this->segmentsAsCamelCase[$this->position] ?? null;
+	}
+
+	public function currentAsSnakeCase(): ?string
+	{
+		return $this->segments[$this->position] ?? null;
+	}
+
+	public function valid(): bool
+	{
+		return isset($this->segments[$this->position]);
+	}
+
+	/** @return string[] */
+	public function remaining(): array
+	{
+		return $this->remainingAsSnakeCase();
+	}
+
+	/** @return string[] */
+	public function remainingAsCamelCase(): array
+	{
+		return array_slice($this->segmentsAsCamelCase, $this->position);
+	}
+
+	/** @return string[] */
+	public function remainingAsSnakeCase(): array
+	{
+		return array_slice($this->segments, $this->position);
+	}
+
+	public function hasRemainingSegments(): bool
+	{
+		return count($this->remainingAsSnakeCase()) > 0;
+	}
+
+	public function count(): int
+	{
+		return count($this->segments);
+	}
+
+	public function __toString(): string
 	{
 		return $this->path;
 	}
 
-	/**
-	 * Resolve the reference against the given schema.
-	 */
-	public function resolve(object|array $schema): mixed
+	private function assertPositionInBounds(int $index = -1): void
 	{
-		$path = $this->path;
-
-		if ($path === '#') {
-			return $schema;
+		if ($index === -1) {
+			$index = $this->position;
 		}
 
-		if (str_starts_with($path, '#/')) {
-			return $this->resolveAbsolute($schema);
+		if ($index < 0 || $index >= count($this->segments)) {
+			throw new OutOfBoundsException("Index $index is out of bounds for scope path '{$this->path}'");
 		}
-
-		throw new \RuntimeException('Referencing by URL is not currently supported: ' . $path);
-	}
-
-	public function resolveAbsolute(SchemaFacade $schema): mixed
-	{
-		$path = $this->path;
-
-		if ($path === '#') {
-			return $schema;
-		}
-
-		$parts = explode('/', ltrim($path, '#/'));
-		$target = $schema;
-		$targetIsField = false;
-
-
-		foreach ($parts as $part) {
-			// if ($targetIsField) {
-			// 	if ($part === 'value') {
-			// 		return $target->attributes->findByName($part)?->defaultsTo($target->value);
-			// 	}
-			// 	return $target->attributes->findByName($part);
-			// }
-
-			if (is_object($target) && isset($target->$part)) {
-				$target = $target->$part;
-				if ($target instanceof Field) {
-					$targetIsField = true;
-				}
-				continue;
-			}
-
-			if (is_array($target) && isset($target[$part])) {
-				$target = $target[$part];
-				continue;
-			}
-
-			throw new \InvalidArgumentException('Invalid reference: ' . $path);
-		}
-
-		// var_dump($path, $target);die;
-
-		return $target;
-	}
-
-	/**
-	 * The same as resolve() but will backtrack up the path until it finds a valid reference.
-	 */
-	public function resolveWithBackTracking(SchemaFacade $schema): mixed
-	{
-		$path = $this->path;
-
-		if ($path === '#') {
-			return $schema;
-		}
-
-		$parts = explode('/', ltrim($path, '#/'));
-		$target = $schema;
-		$targets = [];
-
-		foreach ($parts as $part) {
-			if (is_object($target) && isset($target->$part)) {
-				$targets[] = $target->$part;
-				$target = $target->$part;
-				continue;
-			}
-
-			if (is_array($target) && isset($target[$part])) {
-				$targets[] = $target[$part];
-				$target = $target[$part];
-				continue;
-			}
-
-			$targets[] = $target;
-			break;
-		}
-
-		array_pop($targets);
-
-		$target = $targets[count($targets)-1];
-
-		if ($target) {
-			return $target;
-		}
-
-		throw new \InvalidArgumentException('Invalid reference: ' . $path);
-	}
-
-	/**
-	 * Return the last segment of the path.
-	 */
-	public function getLastSegment(): string
-	{
-		$parts = explode('/', ltrim($this->path, '#/'));
-
-		return array_pop($parts);
 	}
 }
